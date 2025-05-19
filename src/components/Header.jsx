b9c80a8e8d4ca2ceba6b172532b960ef5627f9dc
delacroix <delacroix.jon@gmail.com>
2025-05-19 11:56:09 +0200

    logo + menu

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Menu from './Menu.jsx'
+
+function Header() {
+
+  return (
+    <>
+        <img src="/src/assets/images/man-3262834_960_720.webp" alt="" srcset="" />
+        <Menu />
+    </>
+  )
+}
+
+export default Header
---
+++ src/components/Menu.jsx
@@ -0,0 +1,12 @@
+
+function Menu() {
+    return (
+        <>
+            <nav>
+                <li to="/">Accueil</li>
+            </nav>
+        </>
+    )
+}
+
+export default Menu
