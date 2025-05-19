accec3896e9600b68918a07e466ddb75f90ed889
delacroix <delacroix.jon@gmail.com>
2025-05-19 13:33:32 +0200

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,12 @@
 function Header() {
 
   return (
-    <>
-        <header>
-            <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="" srcset="" />
-            <nav>
-                <li><a href="/">Accueil</a></li>
-            </nav>
-        </header>
-    </>
+    <header>
+        <img src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png" alt="" srcset="" />
+        <nav>
+            <li><a href="/">Accueil</a></li>
+        </nav>
+    </header>
   )
 }
 
