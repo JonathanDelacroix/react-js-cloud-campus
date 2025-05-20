7869e343567aea536dc18a9e3fe2b8125dfb9b1a
delacroix <delacroix.jon@gmail.com>
2025-05-20 10:24:59 +0200

    Badge IsNew

--- src/App.jsx
+++ src/App.jsx
@@ -16,16 +16,19 @@ function App() {
               image="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
               title="Tacos à l’unité"
               price={3}
+              isNew={true}
             />
             <Dish
               image="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
               title="Enchiladas"
               price={12}
+              isNew={false}
             />
             <Dish
               image="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
               title="Mole poblano"
               price={15}
+              isNew={false}
             />
           </Row>
         </Container>
--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,12 +1,18 @@
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import '../assets/styles/dish.scss';
+import Badge from 'react-bootstrap/Badge';
 
-function Dish ({image,title,price}) {
+function Dish ({image,title,price, isNew}) {
     return (
         <Col md={4}>
             <Card>
-                <Card.Img src={image} alt={title} />
+                <div className="position-relative">
+                    <Card.Img src={image} alt={title} />
+                    {isNew &&
+                    <Badge bg="primary" className="position-absolute top-0 end-0 m-2">Nouveau</Badge>
+                    }
+                </div>
                 <Card.Body>
                 <Card.Title>{title}</Card.Title>
                 <Card.Text>
