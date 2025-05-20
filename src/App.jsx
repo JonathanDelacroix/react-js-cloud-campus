e8a848e47ddfe63db807f52707d9086729d28734
delacroix <delacroix.jon@gmail.com>
2025-05-20 15:14:37 +0200

    Bouton Nouveautés uniquement

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,44 @@ import Footer from './components/Footer.jsx';
 import Dish from './components/Dish.jsx';
 import { Container, Row } from 'react-bootstrap';
 import './assets/styles/main.scss';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 
 function App() {
 
   const dishes = [
     {image: "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg", title: "Tacos à l’unité", price: 3, isNew: true, stock: 12},
     {image: "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg", title: "Enchiladas", price: 12, isNew: false, stock: 0},
-    {image: "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg", title: "Mole poblano", price: 15, isNew: false, stock: 5}
+    {image: "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg", title: "Mole poblano", price: 15, isNew: false, stock: 5},
   ]
 
-  const filteredDishes = dishes.filter(dish => dish.stock > 0);
+  const [showNewOnly, setNewOnly] = useState(false);
+
+  const handleShowNewOnly = () => {
+    setNewOnly(prev => !prev);
+  }
+
+  const filteredDishes = dishes.filter(dish => dish.stock > 0 && (!showNewOnly || dish.isNew));
 
   return (
     <>
       <Header />
-
-        <Container as="main">
-          <Row>
-            {filteredDishes.map((dish,index) =>(
-                <Dish
-                  key={index}
-                  image={dish.image}
-                  title={dish.title}
-                  price={dish.price}
-                  isNew={dish.isNew}
-                />
-            ))}
-          </Row>
-        </Container>
+      <Button variant="primary" onClick={() => handleShowNewOnly()}>
+        {showNewOnly ? 'Voir tous les plats' : 'Nouveautés uniquement'}
+      </Button>
+      <Container as="main">
+        <Row>
+          {filteredDishes.map((dish,index) =>(
+              <Dish
+                key={index}
+                image={dish.image}
+                title={dish.title}
+                price={dish.price}
+                isNew={dish.isNew}
+              />
+          ))}
+        </Row>
+      </Container>
 
       <Footer />
     </>
